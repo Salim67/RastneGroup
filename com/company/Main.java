f6253f947c505401632d29422fdbde41563dc59a
Salim67 <30494310+Salim67@users.noreply.github.com>
2017-08-01 10:04:24 +0200

    initial commit

---
+++ com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+       String name, age;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name");
+        name = input.nextLine();
+        System.out.println("Enter age");
+        age = input.nextLine();
+        System.out.println("your name is"+ name);
+        System.out.println("your age is"+ age);
+    }
+}
